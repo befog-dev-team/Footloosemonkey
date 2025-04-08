c7bba75a039d35895cb5e7f55876ded41741c150
Anuj3553 <anujverma3553@gmail.com>
2025-04-08 14:36:51 +0530

    refactor: Update AdminData model fields and adjust seed script for admin data seeding

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,14 +3,30 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.admin.create({
-        data: {
-            email: 'befogdev@gmail.com',
-            password: 'Befog@123456',
-        },
-    });
+    // For seeding user data, uncomment the following lines and adjust as needed
 
-    console.log('✅ Admin seeded successfully');
+    // await prisma.admin.create({
+    //     data: {
+    //         email: 'befogdev@gmail.com',
+    //         password: 'Befog@123456',
+    //     },
+    // });
+
+    // console.log('✅ Admin seeded successfully');
+
+    // For seeding admin data, uncomment the following lines and adjust as needed
+
+    // await prisma.adminData.create({
+    //     data: {
+    //         talent: 'Dancing',
+    //         offerCharge: '500',
+    //         groupACharge: '1000',
+    //         groupBCharge: '1500',
+    //         groupCCharge: '2000',
+    //     },
+    // });
+
+    // console.log('✅ Admin Data seeded successfully');
 }
 
 main()
